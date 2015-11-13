21d4ab9da5e4aea0203e21dbcb0a08774f4d60de
norips <raphdruon@gmail.com>
2015-11-13 14:46:15 +0100

    Update .travis-deps.sh

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -2,10 +2,6 @@
 set -e
 set -x
 
-# Build and install devkitARM + ctrulib
-wget http://sourceforge.net/projects/devkitpro/files/Automated%20Installer/devkitARMupdate.pl
-git clone https://github.com/smealum/ctrulib.git
-sudo perl devkitARMupdate.pl
 
 # Get latest ctrulib and overwrite bundled one
 cd ctrulib/libctru && make ; cd -
