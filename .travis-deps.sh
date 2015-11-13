89872ba3b4ad091c8229aac9149001b265d3a04d
norips <raphdruon@gmail.com>
2015-11-13 12:53:06 +0100

    Update .travis-deps.sh

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -16,4 +16,4 @@ wget http://sourceforge.net/projects/giflib/files/giflib-5.1.1.tar.bz2/download
 tar -xvf download
 cd giflib-5.1.1
 ./configure --prefix=${PORTLIBS} --host=arm-none-eabi --disable-shared --enable-static
-make && make install
+make && sudo make install
