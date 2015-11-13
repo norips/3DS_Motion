0925cc171e739481bcaa97896e6d8cbf8f10d966
norips <raphdruon@gmail.com>
2015-11-13 15:06:42 +0100

    Update .travis-deps.sh

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -12,4 +12,4 @@ sudo cp -rf ctrulib/libctru/ ${DEVKITPRO}
 git clone https://github.com/norips/3ds_portlibs.git
 cd 3ds_portlibs
 wget -O giflib-5.1.1.tar.bz2 http://sourceforge.net/projects/giflib/files/giflib-5.1.1.tar.bz2/download
-make giflib && sudo make install
+make giflib && make install
